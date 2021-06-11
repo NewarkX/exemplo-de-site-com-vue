3890fce78e94b355993cef9483c330ef2f430e99
NewarkX <brunowmanha@gmail.com>
2021-06-11 16:30:22 +0000

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,52 @@
+<template>
+    <main>
+        <div class="text-top">
+            Vídeos novos toda quinta
+        </div>
+
+        <a href="https://youtube.com/wdevoficial" target="blank">
+            <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/subscribe.svg
+" alt="inscreva-se" id="subscribe">
+        </a>
+
+        <div class="text-bottom">
+            youtube.com/wdevoficial
+        </div>
+    </main>
+</template>
+
+<script>
+    export default{
+        name: 'Home'
+    }
+</script>
+
+<style scoped>
+    main{
+        background-color: var(--color-background-home);
+        justify-content: center;
+        align-items: center;
+    }
+
+    .text-top{
+        color: var(--color-text-light);
+        font-size: 22px;
+        margin-bottom: 20px;
+    }
+
+    .text-bottom{
+        color: var(--color-text-light);
+        font-size: 18px;
+        margin-top: 20px;
+    }
+
+    @media (min-width:700px){
+        #subscribe{
+            width: 370px;
+        }
+    }
+
+    #subcribe{
+        width: 340px;
+    }
+</style>
